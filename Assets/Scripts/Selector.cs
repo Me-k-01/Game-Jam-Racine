d0a801411788b9fb2ca95d064afe794a92155938
Romain <siaudeau.romain@orange.fr>
2023-02-04 20:44:00 +0100

    update

--- Assets/Scripts/Selector.cs
+++ Assets/Scripts/Selector.cs
@@ -9,7 +9,8 @@ public class Selector : MonoBehaviour {
     public int gridSizeHeight = 6;
 
     // Start is called before the first frame update
-    void Start() { 
+    void Start() {
+        var cam=GetComponent<Camera>(); 
     }
 
     // Update is called once per frame
