70f082a26b385c8248e9d3adc49aa2813971d0da
turfa00 <fabrice.turikumwe@etudiant.univ-perp.fr>
2023-02-05 02:06:35 +0100

    FabriceScene

--- Assets/setUpTerrain.cs
+++ Assets/setUpTerrain.cs
@@ -6,27 +6,38 @@ public class setUpTerrain : MonoBehaviour
 {
     public int nbrCases;
     public float width, height;
+    public int gridWidth, gridHeight;
     public Vector3 offset, objectPosition;
     public float w_step, h_step;
     public List <GameObject> terrainObjects = new List<GameObject> ();
+    public GameObject[,] terrainObject;
+
+    //Demo
+    public float[,] positions;
     // Start is called before the first frame update
     void Start()
     {
         width = Mathf.Max(transform.localScale.x, transform.localScale.y) * 10f;
         height = Mathf.Max(transform.localScale.x, transform.localScale.y) * 10f;
         nbrCases = 8;
+        //GridSize
+        gridWidth = 7;
+        gridHeight = 7;
+        //positions = new float[gridHeight, gridWidth];
 
+        terrainObject = new GameObject[gridHeight,gridWidth];
         w_step = width / nbrCases;
         h_step = height / nbrCases;
         offset = new Vector3(-10f, 10f, 0f);
         objectPosition = transform.position + offset;
 
         List<Vector3> objectsPositions = new List<Vector3>();
-        for (int i = 0; i < nbrCases; i++) 
+        for (int i = 0; i < gridWidth; i++) 
         {
-            for(int j = 0; j < nbrCases; j++)
+            for(int j = 0; j < gridHeight; j++)
             {
                 objectsPositions.Add(objectPosition);
+                //positions[i][j] = objectPosition;
                 objectPosition = new Vector3(objectPosition.x + w_step, objectPosition.y, 0f);
             }
             objectPosition = new Vector3(offset.x, objectPosition.y - h_step, 0f);
