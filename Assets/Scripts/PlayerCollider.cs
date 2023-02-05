cd01d29c560d121313bd61710221c8dc66c4bbb8
Romain <siaudeau.romain@orange.fr>
2023-02-05 14:43:17 +0100

    UI

--- Assets/Scripts/PlayerCollider.cs
+++ Assets/Scripts/PlayerCollider.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerCollider : MonoBehaviour {
     public PlayerStats playerStats;
+
+    private VieManager vieManager;
+    private EauManager eauManager;
     // Start is called before the first frame update
     void Start() {
-        
+        vieManager= GameObject.Find("Canvas").GetComponent<VieManager>();
+        eauManager= GameObject.Find("Canvas").GetComponent<EauManager>();
     }
 
     // Update is called once per frame
@@ -16,19 +20,24 @@ public class PlayerCollider : MonoBehaviour {
     void OnCollisionEnter (Collision collision) {
         if(collision.collider.tag == "Magma")  {
             playerStats.eau -= 3;
+            eauManager.eau = playerStats.eau;
             Debug.Log("Collision Magma");
         }
         if(collision.collider.tag=="Insecte") {
             playerStats.vie -= 2;
+            vieManager.vie = playerStats.vie;
             Debug.Log("Collision Insecte");
         }
         if(collision.collider.tag=="Roche") {
             playerStats.eau --;
             playerStats.vie --;
+            vieManager.vie = playerStats.vie;
+            eauManager.eau = playerStats.eau;
             Debug.Log("Collision Roche");
         }
         if(collision.collider.tag=="Eau") {
-            playerStats.eau += 10; 
+            playerStats.eau += 10;
+            eauManager.eau = playerStats.eau;
             Debug.Log("Collision Eau");
         }
     }
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour {
     public int vie;
     public int eau;
+
+    private VieManager vieManager;
+    private EauManager eauManager;
     // Start is called before the first frame update
     void Start() {
+        vieManager= GameObject.Find("Canvas").GetComponent<VieManager>();
+        eauManager= GameObject.Find("Canvas").GetComponent<EauManager>();
         vie=10;
         eau=10;
+
+        vieManager.vie = vie;
+        eauManager.eau =eau;
     }
 
     // Update is called once per frame 
 
     public void Moved() {
         eau --;
+        eauManager.eau -= 1f;
     }
 
     void game_over()
